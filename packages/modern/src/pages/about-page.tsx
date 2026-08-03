3b5fce0c0427ee18581165186af23e953791a6ca
Alex Iaria <skewy_banisters@aleeas.com>
2026-08-03 19:08:09 -0400

    Added some copy

--- packages/modern/src/pages/about-page.tsx
+++ packages/modern/src/pages/about-page.tsx
@@ -26,37 +26,40 @@ function ModernAboutPage() {
   return (
     <Container
       data-component="modern-about-page"
-      className="max-w-5xl h-screen mx-auto grid items-center"
+      // className="max-w-5xl h-screen mx-auto grid items-center"
+      className="max-w-[1200px] mx-auto grid items-center py-20"
     >
       <div className="grid grid-cols-1 gap-20 md:grid-cols-[auto_1fr]">
-        <div className="flex flex-col gap-10">
-          <div className="flex gap-10 items-center">
-            <div className="overflow-hidden rounded-lg h-[100px] w-[100px] flex items-center justify-center">
-              <img
-                src={about.image.src}
-                alt={about.image.alt}
-                height={about.image.height}
-                width={about.image.width}
-                className="object-cover w-full h-full object-center block"
-              />
+        <div className="grid gap-10 items-center h-[70%]">
+          <div className="flex sticky top-[100px] flex-col gap-10">
+            <div className="flex gap-10 items-center">
+              <div className="overflow-hidden rounded-lg h-[100px] w-[100px] flex items-center justify-center">
+                <img
+                  src={about.image.src}
+                  alt={about.image.alt}
+                  height={about.image.height}
+                  width={about.image.width}
+                  className="object-cover w-full h-full object-center block"
+                />
+              </div>
+              <h1 className="text-6xl">{about.title}</h1>
             </div>
-            <h1 className="text-6xl">{about.title}</h1>
-          </div>
-          <div>
-            <Button
-              asChild
-              variant="link"
-              className="pointer p-0 text-xl font-bold transition-all duration-500 decoration-accent"
-            >
-              <a href="/contact">
-                {t('about.ctaLabel')}
-                <ArrowUpRightIcon className="ml-2 inline-block h-4 w-4" />
-              </a>
-            </Button>
+            <div>
+              <Button
+                asChild
+                variant="link"
+                className="pointer p-0 text-xl font-bold transition-all duration-500 decoration-accent"
+              >
+                <a href="/contact">
+                  {t('about.ctaLabel')}
+                  <ArrowUpRightIcon className="inline-block h-4 w-4 text-accent bold" />
+                </a>
+              </Button>
+            </div>
           </div>
         </div>
         <div className="flex flex-col">
-          <div className="grid grid-cols-1 gap-10">
+          <div className="grid grid-cols-1 gap-10 pt-20 pb-20">
             <div className="text-3xl font-bold flex gap-4 items-center">
               <span>{about.name}</span>
               <span className="text-accent animate-caret-blink duration-100">
@@ -67,6 +70,7 @@ function ModernAboutPage() {
               <p>{about.summary}</p>
             </div>
             <div
+              className="prose py-4"
               dangerouslySetInnerHTML={{
                 __html: DOMPurify.sanitize(about.full),
               }}
--- packages/modern/src/pages/experience-page.tsx
+++ packages/modern/src/pages/experience-page.tsx
@@ -12,22 +12,24 @@ function ModernExperiencePage() {
       className="max-w-[1200px] mx-auto grid items-center py-20"
     >
       <div className="grid grid-cols-1 items-start gap-20 md:grid-cols-[300px_1fr]">
-        <div className="sticky top-10 flex flex-col gap-10">
-          <h1 className="text-6xl">{t('experience.heading')}</h1>
-          <p className="text-2xl font-medium text-accent">
-            {t('experience.subtitle')}
-          </p>
-          <div>
-            <Button
-              asChild
-              variant="link"
-              className="pointer p-0 text-xl font-bold transition-all duration-500 decoration-accent"
-            >
-              <a href="/contact">
-                {t('experience.ctaLabel')}
-                <ArrowUpRightIcon className="ml-2 inline-block h-4 w-4" />
-              </a>
-            </Button>
+        <div className="grid gap-10 items-center h-[70%]">
+          <div className="flex sticky top-[100px] flex-col gap-10">
+            <h1 className="text-6xl">{t('experience.heading')}</h1>
+            <p className="text-2xl font-medium text-accent">
+              {t('experience.subtitle')}
+            </p>
+            <div>
+              <Button
+                asChild
+                variant="link"
+                className="pointer p-0 text-xl font-bold transition-all duration-500 decoration-accent"
+              >
+                <a href="/contact">
+                  {t('experience.ctaLabel')}
+                  <ArrowUpRightIcon className="inline-block h-4 w-4 text-accent" />
+                </a>
+              </Button>
+            </div>
           </div>
         </div>
         <ExperienceList experiences={experience} />
